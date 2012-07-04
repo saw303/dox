1c5cc4baabd077b0d5740ae7f7ca202030e22790
Silvio Wangler <silvio.wangler@gmail.com>
2012-07-04 19:08:10 +0200

    Added additional test for generating SHA-256 hashes

--- core/src/main/java/ch/silviowangler/dox/HashGenerator.java
+++ core/src/main/java/ch/silviowangler/dox/HashGenerator.java
@@ -3,6 +3,7 @@ package ch.silviowangler.dox;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
 
 import java.io.*;
 
@@ -12,9 +13,14 @@ import java.io.*;
  */
 public class HashGenerator {
 
-    Logger logger = LoggerFactory.getLogger(getClass());
+    private static Logger logger = LoggerFactory.getLogger(HashGenerator.class);
 
     public static String sha256Hex(File file) throws IOException {
+
+        Assert.notNull(file, "file must not be null");
+
+        logger.debug("About to generate SHA-256 hash for file '{}'. Does it exist? {}", file.getAbsolutePath(), file.exists());
+
         InputStream inputStream = new FileInputStream(file);
         return  DigestUtils.sha256Hex(inputStream);
     }
--- core/src/test/java/ch/silviowangler/dox/HashGeneratorTest.java
+++ core/src/test/java/ch/silviowangler/dox/HashGeneratorTest.java
@@ -3,6 +3,7 @@ package ch.silviowangler.dox;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -16,14 +17,22 @@ public class HashGeneratorTest {
 
     @Test
     public void generateSha256HexString() throws IOException {
-
-        URL resource = getClass().getClassLoader().getResource("hello.txt");
-
-        String hexHash = HashGenerator.sha256Hex(FileUtils.toFile(resource));
-
-        System.out.println(hexHash.length());
-
+        final File file = loadFile("hello.txt");
+        String hexHash = HashGenerator.sha256Hex(file);
+        assertEquals(64, hexHash.length());
         assertEquals("1f726f5ebbd97a8879f6f4fa8e2f63bd199609d8af4ae022ec42037c2e87d528", hexHash);
+    }
 
+    private File loadFile(String fileName) {
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        return FileUtils.toFile(resource);
+    }
+
+    @Test
+    public void generateAnotherSha256HexString() throws IOException {
+        final File file = loadFile("hello2.txt");
+        String hexHash = HashGenerator.sha256Hex(file);
+        assertEquals(64, hexHash.length());
+        assertEquals("51dabdb8d7e347706260691e3c3447f51c7e747f6b15ddcffd5b9c2ed15ddef7", hexHash);
     }
 }
