b6e737c7a73d0edac3f505910e79a2c51d13c9ac
Silvio Wangler <silvio.wangler@gmail.com>
2012-07-15 20:16:31 +0200

    Improved Javadoc comments

--- api/src/main/java/ch/silviowangler/dox/api/DocumentService.java
+++ api/src/main/java/ch/silviowangler/dox/api/DocumentService.java
@@ -20,8 +20,8 @@ public interface DocumentService {
     /**
      * Implement a search à la Google. Uses a single String to find document references.
      *
-     * @param queryString
-     * @return
+     * @param queryString a simple query string (e.g. a keyword 'taxes' or a date '01.12.2012')
+     * @return all documents that contain that are holding that keyword as a part of an index
      */
     Set<DocumentReference> findDocumentReferences(String queryString);
 }
