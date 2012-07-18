cfe637cbba375b58f26bcc4c78a079a016d1fb65
Silvio Wangler <silvio.wangler@gmail.com>
2012-07-18 12:23:47 +0200

    Added copyright text

--- core/src/main/java/ch/silviowangler/dox/HashGenerator.java
+++ core/src/main/java/ch/silviowangler/dox/HashGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Silvio Wangler (silvio.wangler@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ch.silviowangler.dox;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -5,7 +21,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Silvio Wangler
@@ -22,6 +41,6 @@ public class HashGenerator {
         logger.debug("About to generate SHA-256 hash for file '{}'. Does it exist? {}", file.getAbsolutePath(), file.exists());
 
         InputStream inputStream = new FileInputStream(file);
-        return  DigestUtils.sha256Hex(inputStream);
+        return DigestUtils.sha256Hex(inputStream);
     }
 }
--- web/src/main/webapp/WEB-INF/views/layout.jsp
+++ web/src/main/webapp/WEB-INF/views/layout.jsp
@@ -1,6 +1,22 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
 <%@taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%--
+  ~ Copyright 2012 Silvio Wangler (silvio.wangler@gmail.com)
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~        http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  --%>
+
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="de"> <![endif]-->
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="de"> <![endif]-->
