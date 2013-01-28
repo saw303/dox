0c17ed2c06baa7251901254d88ee7229581cad27
Silvio Wangler <silvio.wangler@gmail.com>
2013-01-28 21:22:53 +0100

    Reverted to 1e5348a

---
+++ web/src/main/java/ch/silviowangler/dox/web/HomeController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 - 2013 Silvio Wangler (silvio.wangler@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.silviowangler.dox.web;
+
+import ch.silviowangler.dox.api.DocumentReference;
+import ch.silviowangler.dox.api.DocumentService;
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Silvio Wangler
+ * @since 0.1
+ *        <div>
+ *        Date: 16.07.12 09:47
+ *        </div>
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DocumentService documentService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "query.html")
+    public ModelAndView query(@RequestParam("q") String queryString) {
+
+        Set<DocumentReference> documentReferences = documentService.findDocumentReferences(queryString);
+        Map<String, Set<DocumentReference>> model = ImmutableMap.of("documents", documentReferences);
+        return new ModelAndView("result.definition", model);
+    }
+}
