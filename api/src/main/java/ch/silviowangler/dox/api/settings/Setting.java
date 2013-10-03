2ba757938e70311b12816e52a131f3ee2f45e809
Silvio Wangler <silvio.wangler@gmail.com>
2013-10-03 08:25:53 +0200

    Added first rest action on SettingsController to start with the AngularJS

---
+++ api/src/main/java/ch/silviowangler/dox/api/settings/Setting.java
@@ -0,0 +1,66 @@
+package ch.silviowangler.dox.api.settings;
+
+import com.google.common.base.Objects;
+
+import java.io.Serializable;
+
+/**
+ * @author Silvio Wangler
+ * @since 0.3
+ */
+public class Setting implements Serializable {
+
+    private String key;
+    private String value;
+
+
+    public Setting() {
+        super();
+    }
+
+    public Setting(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("key", key)
+                .add("value", value)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Setting)) return false;
+
+        Setting setting = (Setting) o;
+
+        if (key != null ? !key.equals(setting.key) : setting.key != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return key != null ? key.hashCode() : 0;
+    }
+}
--- web/src/main/java/ch/silviowangler/dox/web/SettingsController.java
+++ web/src/main/java/ch/silviowangler/dox/web/SettingsController.java
@@ -1,12 +1,16 @@
 package ch.silviowangler.dox.web;
 
+import ch.silviowangler.dox.api.settings.Setting;
 import ch.silviowangler.dox.api.settings.SettingsService;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 import static ch.silviowangler.dox.api.settings.SettingsConstants.SETTING_FIND_ONLY_MY_DOCUMENTS;
@@ -49,5 +53,20 @@ public class SettingsController {
         model.put("fomd", userSettings.containsKey(SETTING_FIND_ONLY_MY_DOCUMENTS) ? userSettings.get(SETTING_FIND_ONLY_MY_DOCUMENTS) : "0");
         model.put("wq", userSettings.containsKey(SETTING_WILDCARD_QUERY) ? userSettings.get(SETTING_WILDCARD_QUERY) : "0");
         return model;
+    }
+
+    @RequestMapping(method = GET, value = "settings")
+    public
+    @ResponseBody
+    List<Setting> listSettings() {
+        final Map<String, String> model = getModel();
+
+        List<Setting> settings = Lists.newArrayListWithCapacity(model.size());
+
+        for (String key : model.keySet()) {
+            settings.add(new Setting(key, model.get(key)));
+        }
+
+        return settings;
     }
 }
