0f77a7925394ddb1190efccefcfa40cc5bd71657
Silvio Wangler <silvio.wangler@gmail.com>
2018-01-12 15:58:40 +0100

    Simple basic auth enable

--- web/src/main/java/ch/silviowangler/dox/DoxApplication.java
+++ web/src/main/java/ch/silviowangler/dox/DoxApplication.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,8 @@
  */
 package ch.silviowangler.dox;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,12 +25,17 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import javax.sql.DataSource;
+import java.io.File;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+import static org.springframework.util.Assert.hasText;
+import static org.springframework.util.Assert.isTrue;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class DoxApplication {
+
+    private final static Logger log = LoggerFactory.getLogger(DoxApplication.class);
 
     @Bean
     @Profile("dev")
@@ -44,6 +51,23 @@ public class DoxApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(DoxApplication.class, args);
+        String doxStore = System.getenv("DOX_STORE");
+
+        hasText(doxStore, "Archive directory must not be null. Please make sure you have properly set environment variable DOX_STORE");
+        File doxStoreFile = new File(doxStore);
+
+        isTrue(doxStoreFile.exists(), String.format("DOX_STORE folder %s does not exist", doxStore));
+
+        File thumbnailsFolder = new File(doxStoreFile, "thumbnails");
+
+        if (!thumbnailsFolder.exists() && doxStoreFile.canWrite() && System.getProperty("spring.profiles.active").equals("dev")) {
+            log.info("You are running in development mode. Creating missing thumnail folder {}", thumbnailsFolder.getAbsolutePath());
+            thumbnailsFolder.mkdir();
+        }
+
+        isTrue(thumbnailsFolder.exists(), String.format("Thumbails folder %s does not exist.", thumbnailsFolder.getAbsolutePath()));
+
+        SpringApplication springApplication = new SpringApplication(DoxApplication.class);
+        springApplication.run(args);
     }
 }
--- web/src/main/java/ch/silviowangler/dox/WebSecurityConfig.java
+++ web/src/main/java/ch/silviowangler/dox/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,12 +17,15 @@ package ch.silviowangler.dox;
 
 import ch.silviowangler.dox.security.DoxUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.security.EndpointRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -31,15 +34,48 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
-                .anyRequest().hasRole("USER")
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("USER")
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .realmName("DOX");
+
+        /*http.httpBasic();
+
+        http.formLogin().loginPage("/login")
+                .loginProcessingUrl("/login/verify")
+                .usernameParameter("username")
+                .passwordParameter("pwd");
+
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/login*").anonymous()
+                .antMatchers("/favicon.ico").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/human.txt").permitAll()
+                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));*/
+    }
+
+    @Bean
+    public AnonymousAuthenticationFilter anonymousAuthenticationFilter() {
+        return new AnonymousAuthenticationFilter("dox");
+    }
+
+    @Bean
+    public AnonymousAuthenticationProvider anonymousAuthenticationProvider() {
+        return new AnonymousAuthenticationProvider("dox");
     }
 
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth, DoxUserDetailService userDetailService) throws Exception {
-        auth.userDetailsService(userDetailService);
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
